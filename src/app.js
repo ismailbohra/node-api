96ac1e3a4615eb2be9624d2c869ddbe621cac05a
ismail bohra <ismailbohra99@gmail.com>
2022-09-21 15:00:46 +0530

    jwt demo implemented

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express=require("express")
 const app=express()
 const morgan=require("morgan")
 const helmet=require("helmet")
+const jwt=require("jsonwebtoken")
 
 const port=process.env.port|| 5000
 require("./util/config")
@@ -27,3 +28,41 @@ app.listen(port,()=>{
     
     console.log(`listening at port ${port}`)
 })
+
+app.post("/api/login",(req,res)=>{
+  const user={
+    id: 1,
+    username:"ismail",
+    email:"ismail@gmail.com"
+  }
+
+  jwt.sign({user}, 'secretkey', (err, token) => {
+    res.json({
+      token
+    });
+  });
+})
+app.post('/api/posts', verifyToken, (req, res) => {  
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if(err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'Post created...',
+        authData
+      });
+    }
+  });
+});
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers['authorization'];
+  if(typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+
+}
