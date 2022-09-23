e03acc86ab8596e0e3d9323cd8dd1d28f7ae9258
ismail bohra <ismailbohra99@gmail.com>
2022-09-23 15:59:38 +0530

    jwt implemented

--- src/routes/student.route.js
+++ src/routes/student.route.js
@@ -5,6 +5,11 @@ const path=require("path")
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerJSDocs = YAML.load(path.join(__dirname,"../util/yaml/studentapi.yaml"));
+// var options = {
+//     swaggerOptions: {
+//       authAction :{ JWT: {name: "JWT", schema: {type: "apiKey", in: "header", name: "Authorization", description: ""}, value: "Bearer <JWT>"} }
+//     }
+//   };
 
 const controller=require("../controller/index")
 
@@ -12,12 +17,12 @@ const getStudent=controller.student_controller
 const validator=require("../validation/student.validation")
 const verifyToken=require("../util/jwtVerification/tokenVerificaton")
 
-router.get("/",getStudent.findAll)
+router.get("/",verifyToken,getStudent.findAll)
 router.post('/createStudent',validator,getStudent.create)
 router.get("/:id",verifyToken,getStudent.findOne)
 router.post("/authenticate",getStudent.auth)
-router.delete("/delete/:id",getStudent.delete)
-router.put("/update/:id",getStudent.update)
+router.delete("/delete/:id",verifyToken,getStudent.delete)
+router.put("/update/:id",verifyToken,getStudent.update)
 router.post("/student/uploadImage2",getStudent.uploadImage)
 router.get("/student/uploadImage",getStudent.uploadImagePage)
 router.use("/student/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJSDocs))
